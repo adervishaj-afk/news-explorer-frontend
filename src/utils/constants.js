ddf49ac9008f8887cb9b9af67911422679363650
adervish <adervishaj159@gmail.com>
2024-10-31 00:14:03 -0400

    updated BASE_URL newsexplore.twilightparadox.com instead of www.newsexplore.twilightparadox.com

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://www.newsexplore.twilightparadox.com"
+    ? "https://newsexplore.twilightparadox.com"
     : "http://localhost:3001";
 
 export { BASE_URL };
